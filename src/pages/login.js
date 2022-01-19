5b42bdbd3fff2890fa6a4a2108b261e03eba3d04
aj-wolftag <arjunalakshan56@gmail.com>
2022-01-19 18:17:02 +0530

    first stage login completed

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,29 +1,39 @@
 import { styled } from "@mui/system";
-import { Button, Grid, Icon, Paper, TextField } from "@mui/material";
-import { borderRadius } from "@mui/system";
+import { Button, Container, Grid, Paper, TextField } from "@mui/material";
 import React from "react";
-import { PhoneAndroidRounded } from "@mui/icons-material";
+import { PhoneAndroidRounded, SendRounded } from "@mui/icons-material";
+import Img from './imgs/bg_building.png';
+
 
 const TextBox = styled(TextField)(() => ({
     '& fieldset': {
       borderRadius: '25px',
     },
   }));
-
+const styles={
+    containerStyle:{
+        minWidth:`100%`,
+        height:`100vh`,
+        backgroundImage:`url(${Img})`,
+        display: `grid`,
+    }
+}
 const Login=()=>{
     return(
-        <Paper elevation={4} sx={{height:'27vh', width:300, padding:'30px',borderRadius:'10px', margin:'25vh auto', opacity:'90%'}}>
-            <TextBox fullWidth label="Enter Username" size="small" sx={{marginBottom: '20px'}}/>
-            <TextBox fullWidth label="Enter Password" size="small" sx={{marginBottom: '20px'}} type='password'/>
-            <TextBox fullWidth label="Enter OTP" size="small" sx={{marginBottom: '10px'}} type='number'/>
-            <Grid align='right'>
-            <Button variant="contained" sx ={{borderRadius:'20px'}} endIcon={<PhoneAndroidRounded/>}>Request OTP</Button>
-            </Grid>
-            <br/>
-            <Grid align='center'>
-            <Button variant="contained" sx ={{borderRadius:'20px'}} >LogIN</Button>
-            </Grid>
-        </Paper>
+        <Container style={styles.containerStyle}>
+            <Paper elevation={4} sx={{height:'250px', width:300, padding:'30px',borderRadius:'10px', margin:'25vh auto', opacity:'90%'}}>
+                <TextBox fullWidth required label="Enter Username" size="small" sx={{marginBottom: '20px'}}/>
+                <TextBox fullWidth required label="Enter Password" size="small" sx={{marginBottom: '20px'}} type='password'/>
+                <TextBox fullWidth label="Enter OTP" size="small" sx={{marginBottom: '10px'}} type='number'/>
+                <Grid align='right'>
+                    <Button variant="contained" sx ={{borderRadius:'20px'}} endIcon={<PhoneAndroidRounded/>}>Request OTP</Button>
+                </Grid>
+                <br/>
+                <Grid align='center'>
+                    <Button variant="contained" color='success' sx ={{borderRadius:'20px'}} endIcon={<SendRounded/>}>LogIN</Button>
+                </Grid>
+            </Paper>
+        </Container>
     );
 }
 
